8dc10a698600404df476b96e31925991132d44a7
tsaristbomba <andrexerdna@gmail.com>
2020-06-29 09:35:56 -0300

    Updates

---
+++ precache-manifest.c74a01e5f68e1a5477444a19c84404ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee4bf2f6182d7e063d30cc042e5371c",
+    "url": "/NoteApp/index.html"
+  },
+  {
+    "revision": "06a833375aaadf536fb8",
+    "url": "/NoteApp/static/css/main.57cea256.chunk.css"
+  },
+  {
+    "revision": "42d820e323e185b09ab9",
+    "url": "/NoteApp/static/js/2.99f12fd8.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/NoteApp/static/js/2.99f12fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a833375aaadf536fb8",
+    "url": "/NoteApp/static/js/main.6997e96e.chunk.js"
+  },
+  {
+    "revision": "cff189bde49f7f0bb32e",
+    "url": "/NoteApp/static/js/runtime-main.56d8437a.js"
+  }
+]);
